61f1b5de82bbdd10b1115c9c20f45cc8a97b59fd
devanshu jain <devanshujain123456789@gmail.com>
2025-02-26 11:58:42 +0530

    p2 solved

--- day1.cpp/p2.cpp
+++ day1.cpp/p2.cpp
@@ -1,2 +1,21 @@
 // problem 2 -> 
 // https://www.geeksforgeeks.org/problems/reverse-bits3556/1?page=1&category=Mathematical&difficulty=Easy&sortBy=submissions
+class Solution {
+  public:
+    long long reversedBits(long long x) {
+     long long ans = 0;  
+     int i=31;
+        while(x>0 && i>=0){
+            int b = x%2;
+            
+       
+               ans+= b* pow(2,i);
+            x=x/2; 
+            i--;
+            
+            
+        }
+    return ans;
+    
+    }
+};
